d0c4c241c0c9267a2568538032aae4cc5d99d02e
MirzaSisic <mirzasisic57@gmail.com>
2019-08-07 21:54:06 +0200

    Passing Properties

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
+    // tslint:disable-next-line: component-selector
     selector: 'mpm-products',
     templateUrl: './products-list.component.html'
 })
 
 export class ProductListComponent {
-
+    pageTitle: string = 'Product list test';
 }
