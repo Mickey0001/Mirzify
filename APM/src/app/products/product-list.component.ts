ee98dba279b7c74252e0a3e8223e8d81028cb205
MirzaSisic <mirzasisic57@gmail.com>
2019-08-12 16:06:11 +0200

    Getters and Setters

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -2,45 +2,55 @@ import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
 
 @Component({
-    // tslint:disable-next-line: component-selector
-    selector: 'mpm-products',
-    templateUrl: './products-list.component.html',
-    styleUrls: ['./product-list.component.css']
+  // tslint:disable-next-line: component-selector
+  selector: 'mpm-products',
+  templateUrl: './products-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
 
 export class ProductListComponent implements OnInit {
-    pageTitle = 'Product list test';
-    imageWidth = 50;
-    imageMargin = 2;
-    showImage = false;
-    listFilter = 'cart';
-    products: IProduct[] = [
-        {
-            'productID': 1,
-            'productName': 'Leaf Rake',
-            'productCode': 'GDN-0011',
-            'releaseDate': 'March 19, 2016',
-            'description': 'Leaf rake with 48-inch wooden handle.',
-            'price': 19.95,
-            'starRating': 3.2,
-            'imageURL': 'https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png'
-          },
-          {
-            'productID': 2,
-            'productName': 'Garden Cart',
-            'productCode': 'GDN-0023',
-            'releaseDate': 'March 18, 2016',
-            'description': '15 gallon capacity rolling garden cart',
-            'price': 32.99,
-            'starRating': 4.2,
-            'imageURL': 'https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png'
-          },
-    ];
-    toggleImage(): void {
-      this.showImage = !this.showImage;
-    }
+  pageTitle = 'Product list test';
+  imageWidth = 50;
+  imageMargin = 2;
+  showImage = false;
 
-    ngOnInit(): void {
-      console.log('Initizalizeeeeeeeeeed');
-    }
+  _listFilter: string;
+  get listFilter(): string {
+    return this._listFilter;
+  }
+
+  set listFilter(value: string) {
+    this._listFilter = value;
+  }
+
+  filteredProducts: IProduct[];
+  products: IProduct[] = [
+    {
+      'productID': 1,
+      'productName': 'Leaf Rake',
+      'productCode': 'GDN-0011',
+      'releaseDate': 'March 19, 2016',
+      'description': 'Leaf rake with 48-inch wooden handle.',
+      'price': 19.95,
+      'starRating': 3.2,
+      'imageURL': 'https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png'
+    },
+    {
+      'productID': 2,
+      'productName': 'Garden Cart',
+      'productCode': 'GDN-0023',
+      'releaseDate': 'March 18, 2016',
+      'description': '15 gallon capacity rolling garden cart',
+      'price': 32.99,
+      'starRating': 4.2,
+      'imageURL': 'https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png'
+    },
+  ];
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+
+  ngOnInit(): void {
+    console.log('Initizalizeeeeeeeeeed');
+  }
 }
